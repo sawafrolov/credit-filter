43f2a6aaedb3b0d44f4a4723b7e783d28e2a48a3
sawafrolov <frolov@devs-vault.com>
2024-01-16 00:19:20 +0400

    Main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
 }
 
 group = "com.github.sawafrolov"
 version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+}
+
+springBoot {
+    buildInfo()
 }
 
 dependencies {
@@ -22,8 +31,16 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
-tasks.getByName<Test>("test") {
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
---
+++ src/main/java/com/github/sawafrolov/creditfilter/CreditFilterApplication.kt
@@ -0,0 +1,12 @@
+package com.github.sawafrolov.creditfilter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditFilterApplication {
+
+    fun main(args: Array<String>) {
+        runApplication<CreditFilterApplication>(*args)
+    }
+}
