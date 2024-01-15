990b76a8dfe20706a4e691b806fe94dcee69e46c
sawafrolov <frolov@devs-vault.com>
2024-01-16 00:08:50 +0400

    Основа проекта

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "com.github.sawafrolov"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.camunda.bpm.dmn:camunda-engine-dmn:7.20.0")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
